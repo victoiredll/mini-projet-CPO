7a1f7cdb09f190c4c6c20972a416657b64b3dd79
victoire <victoire@192.168.0.14>
2020-12-22 14:02:29 +0100

    mise en forme

--- src/mini_projet_cpo/Mini_projet_CPO.java
+++ src/mini_projet_cpo/Mini_projet_CPO.java
@@ -16,6 +16,10 @@ public class Mini_projet_CPO {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //classe joueur
+        // classe grille
+        //class partie
+        //class bouton ?
     }
     
 }
