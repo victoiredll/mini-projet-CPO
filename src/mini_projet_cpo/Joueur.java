dd2b5176b0ee56db07f4620f80e05933f669a812
margo <margo@LAPTOP-GPF3173A.home>
2020-12-22 14:29:06 +0100

    creation classes

---
+++ src/mini_projet_cpo/Joueur.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mini_projet_cpo;
+
+/**
+ *
+ * @author margo
+ */
+public class Joueur {
+    
+    //creation des attributs du joueur
+    String Nom; 
+    double Chrono_Perso; //cet attribut correspondra à la liste des temps du joueur 
+    
+    //creation du constructeur 
+    public Joueur (String nom){
+      Nom=nom; 
+    }
+    
+    //creation des methodes 
+    
+    //Fonction cliquer bouton 
+}
